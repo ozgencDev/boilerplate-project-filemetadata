69b3e7b7782a2c7481eabf5df60b5a690448fe0a
ozgencdev <ozgencdev@gmail.com>
2022-02-11 19:48:52 +0300

    test-2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 var app = express();
 
-const storage = multer.diskStorage({
+/* const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads");
   },
@@ -15,15 +15,14 @@ const storage = multer.diskStorage({
     cb(null, uniqueSuffix + "-" + file.originalname);
   },
 });
-
-const upload = multer({ storage: storage });
+ */
+const upload = multer({ storage: multer.memoryStorage() });
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   const { originalname: name, mimetype: type, size: size } = req.file;
-  console.log(name, type, size);
   res.json({ name, type, size });
 });
 
